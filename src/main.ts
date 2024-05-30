0cd7448e333a71f8c7fbf9e5fa79b87e85208c91
kiiinhos <viniciuscamargo713@gmail.com>
2024-05-30 13:32:50 -0300

    ajustando main

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
